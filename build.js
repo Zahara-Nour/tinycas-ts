8dfd11dbd9df3f87aa6ace716f91e80e8429edf0
David Le Jolly <zahara.alnour@gmail.com>
2023-01-28 20:16:35 +0300

    fix config

--- build.js
+++ build.js
@@ -1,35 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require('esbuild')
 const { dependencies, peerDependencies } = require('./package.json')
 // Automatically exclude all node_modules from the bundled version
 // const { nodeExternalsPlugin } = require('esbuild-node-externals')
+console.log(dependencies)
+esbuild
+	.build({
+		entryPoints: ['src/index.ts'],
+		outdir: 'dist',
+		bundle: true,
+		sourcemap: true,
+		minify: true,
+		splitting: true,
+		format: 'esm',
+		target: ['esnext'],
+		external: Object.keys(dependencies ? dependencies : {}).concat(
+			Object.keys(peerDependencies ? peerDependencies : {}),
+		),
+	})
+	.then(() => console.log('⚡ Done : esm'))
+	.catch(() => process.exit(1))
 
 esbuild
-  .build({
-    entryPoints: ['src/index.ts'],
-    outdir: 'dist',
-    bundle: true,
-    sourcemap: true,
-    minify: true,
-    splitting: true,
-    format: 'esm',
-    target: ['esnext'],
-    external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
-  })
-  .then(() => console.log('⚡ Done : esm'))
-  .catch(() => process.exit(1))
-
-esbuild
-  .build({
-    entryPoints: ['src/index.ts'],
-    outfile: 'dist/index.cjs',
-    bundle: true,
-    sourcemap: true,
-    minify: true,
-    target: ['esnext'],
-    platform: 'node',
-    // plugins: [nodeExternalsPlugin()]
-  })
-  .then(() => console.log('⚡ Done : node'))
-  .catch(() => process.exit(1))
+	.build({
+		entryPoints: ['src/index.ts'],
+		outfile: 'dist/index.cjs',
+		bundle: true,
+		sourcemap: true,
+		minify: true,
+		target: ['esnext'],
+		platform: 'node',
+		// plugins: [nodeExternalsPlugin()]
+	})
+	.then(() => console.log('⚡ Done : node'))
+	.catch(() => process.exit(1))
 // Output:
 // { errors: [], warnings: [] }
